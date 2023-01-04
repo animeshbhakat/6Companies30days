089bdceea1c570d3cbfd2bc1060f982baf6efe33
Animesh Bhakat <60314168+animeshbhakat@users.noreply.github.com>
2023-01-04 16:49:47 +0530

    added Airplane Seat Assignment Probability solutio

---
+++ 1227AirplaneSeatAssignmentProbability.java
@@ -0,0 +1,11 @@
+class Solution {
+    public double nthPersonGetsNthSeat(int n) {
+        double d = 1.0;
+        if(n == 1){
+            return d;
+        }
+
+        d = 0.5;
+        return d;
+    }
+}
